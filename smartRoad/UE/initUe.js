0cc36ee6e8ccb0ce80779ab92ae4b10779fd988c
zhengweiming <z15827720361wm@163.com>
2022-05-11 23:09:44 +0800

    feat: 长深公路对接接口

--- smartRoad/UE/initUe.js
+++ smartRoad/UE/initUe.js
@@ -1,2 +1,13 @@
-"object"!=typeof ue||"object"!=typeof ue.interface?("object"!=typeof ue&&(ue={}),ue.interface={},ue.interface.broadcast=function(e,t){if("string"==typeof e){var o=[e,""];void 0!==t&&(o[1]=t);var n=encodeURIComponent(JSON.stringify(o));"object"==typeof history&&"function"==typeof history.pushState?(history.pushState({},"","#"+n),history.pushState({},"","#"+encodeURIComponent("[]"))):(document.location.hash=n,document.location.hash=encodeURIComponent("[]"))}}):function(e){ue.interface={},ue.interface.broadcast=function(t,o){"string"==typeof t&&(void 0!==o?e.broadcast(t,JSON.stringify(o)):e.broadcast(t,""))}}(ue.interface),(window.ue4=ue.interface.broadcast);
-window.ue = ue;
+typeof ue !== 'object' || typeof ue.interface !== 'object' ? (typeof ue !== 'object' && (ue = {}), ue.interface = {}, ue.interface.broadcast = function(e, t) {
+  if (typeof e === 'string') {
+    var o = [e, '']
+    void 0 !== t && (o[1] = t)
+    var n = encodeURIComponent(JSON.stringify(o))
+    typeof history === 'object' && typeof history.pushState === 'function' ? (history.pushState({}, '', '#' + n), history.pushState({}, '', '#' + encodeURIComponent('[]'))) : (document.location.hash = n, document.location.hash = encodeURIComponent('[]'))
+  }
+}) : (function(e) {
+  ue.interface = {}, ue.interface.broadcast = function(t, o) {
+    typeof t === 'string' && (void 0 !== o ? e.broadcast(t, JSON.stringify(o)) : e.broadcast(t, ''))
+  }
+}(ue.interface)), (window.ue4 = ue.interface.broadcast)
+window.ue = ue
--- smartRoad/config/config.js
+++ smartRoad/config/config.js
@@ -1,74 +1,22 @@
+const CONFIG_SERVICE = {
+  baseUrl: 'http://1.15.23.25:8080',
+  tempToken: 'eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6IjhmNzEwN2QwLTEyNDQtNDNhNS04NTc1LTg2Y2E3Y2IwZTkyMyJ9.J_kiuHyD01B4EKMzUX7SARPylBkpQEldoW6LIPMqE8IoakMxwEiCnyv8A3daplxnsCyKsbMqgkQzrOBccYJYog'
+}
+
 const CONFIG = {
-    sectionList:[
-        {
-            name: 'S3-1-2 T3',
-            left: 145,
-            top: 31,
-            click: true,
-        },
-        {
-            name: 'S3-2-2 T3',
-            left: 273,
-            top: 5,
-            click: false,
-        },
-        {
-            name: 'S3-3-2 T3',
-            left: 398,
-            top: -10,
-            click: false,
-        },
-        {
-            name: 'S3-4-2 T3',
-            left: 523,
-            top: -15,
-            click: false,
-        },
-        {
-            name: 'S3-5-2 T3',
-            left: 648,
-            top: -20,
-            click: false,
-        },
-        {
-            name: 'S3-6-2 T3',
-            left: 773,
-            top: -15,
-            click: false,
-        },
-        {
-            name: 'S3-7-2 T3',
-            left: 898,
-            top: -10,
-            click: false,
-        },
-        {
-            name: 'S3-8-2 T3',
-            left: 1023,
-            top: 5,
-            click: false,
-        },
-        {
-            name: 'S3-9-2 T3',
-            left: 1148,
-            top: 31,
-            click: false,
-        }
-    ],
-    bimLayers:[
-        '右侧路基___总_2@组装',
-        '广汕公路跨线桥_1_1@广汕公路跨线桥',
-        '东江大桥水面@东江大桥水面',
-        '软土路基@组装',
-        '左侧路基总@组装',
-        '东江大桥@组装',
-        '削坡@组装',
-        '挡墙着色@组装',
-        'CSD路基__广汕公路跨线桥测试_1_1@广汕公路跨线桥-左',
-        '台背回填@组装',
-        '右侧测试路基__1@广汕公路跨线桥-右',
-        '挖台阶@组装'
-    ],
-    tiltLayers: [ 'Combin'],    
-    
+  bimLayers: [
+    '右侧路基___总_2@组装',
+    '广汕公路跨线桥_1_1@广汕公路跨线桥',
+    '东江大桥水面@东江大桥水面',
+    '软土路基@组装',
+    '左侧路基总@组装',
+    '东江大桥@组装',
+    '削坡@组装',
+    '挡墙着色@组装',
+    'CSD路基__广汕公路跨线桥测试_1_1@广汕公路跨线桥-左',
+    '台背回填@组装',
+    '右侧测试路基__1@广汕公路跨线桥-右',
+    '挖台阶@组装'
+  ],
+  tiltLayers: ['Combin']
 }
